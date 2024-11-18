2722cf7cbb771ac229d600d226e168b4a3b8fe73
md.naiem <150327338+naiemoi@users.noreply.github.com>
2024-11-18 18:08:01 +0600

    Add files via upload

---
+++ again_twenty_five.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+int main(){
+    long long n;
+    cin>>n;
+    if(n==0)
+    cout<<"01"<<endl;
+    else if(n==1){
+        cout<<"05"<<endl;
+    } 
+    else{
+        cout<<"25"<<endl;
+    }
+    return 0;
+}
